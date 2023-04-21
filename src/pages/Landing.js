88b7b90e62ba2ee942c5de27eabab59b24c9e1b5
Elias <elias@Eliass-MacBook-Pro.local>
2023-04-21 14:03:38 +0200

    moved repo

---
+++ src/pages/Landing.js
@@ -0,0 +1,33 @@
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+import { Logo } from "../components";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            This project allows you to collect, search for, edit and remove job
+            applications. Also features a small dashboard with stats. Try it
+            out!
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
