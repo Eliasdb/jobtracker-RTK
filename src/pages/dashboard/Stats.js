aa1f7526bf18355175cb892418d06076bc009533
Elias <elias@Eliass-MacBook-Pro.local>
2023-04-21 14:48:27 +0200

    cleaned up warnings

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,16 +1,15 @@
-import { useEffect } from 'react';
-import { StatsContainer, Loading, ChartsContainer } from '../../components';
-import { useDispatch, useSelector } from 'react-redux';
-import { showStats } from '../../features/allJobs/allJobsSlice';
+import { useEffect } from "react";
+import { StatsContainer, ChartsContainer } from "../../components";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
 
 const Stats = () => {
-  const { isLoading, monthlyApplications } = useSelector(
-    (store) => store.allJobs
-  );
+  const { monthlyApplications } = useSelector((store) => store.allJobs);
 
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(showStats());
+    // eslint-disable-next-line
   }, []);
   return (
     <>
